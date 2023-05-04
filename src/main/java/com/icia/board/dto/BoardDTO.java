01a31446bd89bbddc720346c2ff92eeb6a2a2abc
eunsu1 <lu0201@naver.com>
2023-05-04 11:21:56 +0900

    파일 추가전

---
+++ src/main/java/com/icia/board/dto/BoardDTO.java
@@ -0,0 +1,25 @@
+package com.icia.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+public class BoardDTO {
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private Timestamp boardCreatedDate;
+    private int boardHits;
+    private int fileAttached;
+    private MultipartFile boardFile;
+
+
+}
